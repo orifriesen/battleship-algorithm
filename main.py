24d3fc7e205ba2b2478fabd9162cb2474e3f2b24
Ori Friesen <32725393+orifriesen@users.noreply.github.com>
2021-03-13 01:28:16 -0800

    finally fixed everything

--- main.py
+++ main.py
@@ -6,9 +6,13 @@ def verticalCheck(w, algorithmGrid, gameBoard):
 	boatSize = w - 1
 	for x in range(boardSize):
 		for y in range(boardSize - boatSize):
-			print(x, y)
-			if 1 not in gameBoard[y:y+boatSize+1][x]:
-				algorithmGrid[y:y+boatSize+1][x] += 1
+			hitAlready = False
+			for z in range(boatSize+1):
+				if gameBoard[y+z][x] == 1:
+					hitAlready = True
+			if hitAlready == False:
+				for z in range(boatSize+1):
+					algorithmGrid[y+z][x] += 1
 	return algorithmGrid
 
 def horizontalCheck(w, algorithmGrid, gameBoard):
@@ -23,14 +27,14 @@ def hunt(gameBoard, boardSize):
 	algorithmGrid = np.array([[0 for i in range(boardSize)] for j in range(boardSize)])
 	for w in shipTypeList:
 		algorithmGrid = horizontalCheck(w, algorithmGrid, gameBoard)
-		#algorithmGrid = verticalCheck(w, algorithmGrid, gameBoard)
+		algorithmGrid = verticalCheck(w, algorithmGrid, gameBoard)
 	return algorithmGrid
 
 test = hunt(gameBoard, boardSize)
 
 for row in test:
 	print(row)
-
+print()
 for ship in shipList:
 	for coord in ship:
 		i, j = coord
